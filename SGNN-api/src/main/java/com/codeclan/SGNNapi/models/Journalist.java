780a1c49152f6302299404faeb880d38adda4d48
SitharaS <sithu_s@hotmail.com>
2018-10-19 11:21:18 +0100

    Journalist class - constructors, getters-setters ready

--- SGNN-api/src/main/java/com/codeclan/SGNNapi/models/Journalist.java
+++ SGNN-api/src/main/java/com/codeclan/SGNNapi/models/Journalist.java
@@ -1,4 +1,46 @@
 package com.codeclan.SGNNapi.models;
 
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Journalist {
+
+    private Long id;
+
+    private String name;
+
+    private List<Article> articles;
+
+    public Journalist() {
+    }
+
+    public Journalist(String name) {
+        this.name = name;
+        this.articles = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 }
