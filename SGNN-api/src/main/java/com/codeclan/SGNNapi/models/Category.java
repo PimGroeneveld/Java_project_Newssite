a52bf349fd4b68809dce05f23d74e79a3766f022
SitharaS <sithu_s@hotmail.com>
2018-10-19 11:40:09 +0100

    Category class - constructors, getters-setters ready

--- SGNN-api/src/main/java/com/codeclan/SGNNapi/models/Category.java
+++ SGNN-api/src/main/java/com/codeclan/SGNNapi/models/Category.java
@@ -1,4 +1,45 @@
 package com.codeclan.SGNNapi.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Category {
+
+    private Long id;
+
+    private CategoryName name;
+
+    private List<Article> articles;
+
+    public Category() {
+    }
+
+    public Category(CategoryName name) {
+        this.name = name;
+        this.articles = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CategoryName getName() {
+        return name;
+    }
+
+    public void setName(CategoryName name) {
+        this.name = name;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 }
---
+++ SGNN-api/src/main/java/com/codeclan/SGNNapi/models/CategoryName.java
@@ -0,0 +1,12 @@
+package com.codeclan.SGNNapi.models;
+
+public enum CategoryName {
+    POLITICS,
+    ENTERTAINMENT,
+    SPORTS,
+    HEALTH,
+    TECH,
+    EDUCATION,
+    ECONOMY,
+    GENERAL
+}
