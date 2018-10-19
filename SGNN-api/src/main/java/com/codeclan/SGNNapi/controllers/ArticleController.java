8a5648141469149c3aafdbe1c3523d4a114b1a80
SitharaS <sithu_s@hotmail.com>
2018-10-19 14:44:04 +0100

    ArticleController - can find articles by category id

--- SGNN-api/src/main/java/com/codeclan/SGNNapi/controllers/ArticleController.java
+++ SGNN-api/src/main/java/com/codeclan/SGNNapi/controllers/ArticleController.java
@@ -1,4 +1,24 @@
 package com.codeclan.SGNNapi.controllers;
 
+import com.codeclan.SGNNapi.models.Article;
+import com.codeclan.SGNNapi.repositories.articles.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/articles")
 public class ArticleController {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @GetMapping(value = "/category/{categoryId}")
+    public List<Article> findArticlesByCategory(@PathVariable Long categoryId){
+        return articleRepository.findArticlesByCategory(categoryId);
+    }
 }
