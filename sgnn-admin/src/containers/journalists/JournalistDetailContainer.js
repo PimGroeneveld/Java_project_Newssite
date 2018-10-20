11ee68530922db005b944b4f5bfe0afd88626fbd
Pim <pimgroeneveld92@gmail.com>
2018-10-20 12:46:42 +0100

    adds single page for full journalist info

--- sgnn-admin/src/containers/journalists/JournalistDetailContainer.js
+++ sgnn-admin/src/containers/journalists/JournalistDetailContainer.js
@@ -1,7 +1,31 @@
-// renders individual journalist info
-//gets edit and delete button
+import React, { Component } from 'react';
+import JournalistSingleDetail from '../../components/journalists/JournalistSingleDetail';
 
-//render()
-//<Remove journalist button>
-//<edit journalist button>
-//journalist name and image>
+class JournalistDetailContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {journalist: null}
+    this.url = props.url;
+    console.log("journalistSingleDetail props.url", props.url);
+  }
+
+  componentDidMount(){
+    fetch(this.url).then((res) => res.json())
+    .then((data) => {
+      console.log("detailcontainer: componentDidMount data", data)
+      this.setState({journalist: data})
+    })
+  }
+
+  render() {
+    return(
+      // add edit and remove button in this div
+      <div className="journalist-container">
+        <h1>Journalist info:</h1>
+        <JournalistSingleDetail journalist = { this.state.journalist } />
+      </div>
+    )
+  }
+}
+
+export default JournalistDetailContainer;
