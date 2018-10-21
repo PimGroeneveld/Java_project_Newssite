496dae351dc902bbedffe5c02e70b9846ea8aaa6
Pim <pimgroeneveld92@gmail.com>
2018-10-21 14:57:18 +0100

    adds back to all button and delete button which isnt working yet

--- sgnn-admin/src/components/journalists/DeleteJournalistButton.js
+++ sgnn-admin/src/components/journalists/DeleteJournalistButton.js
@@ -1,15 +1,15 @@
-import React from "react";
-// import JournalistDetailContainer from "../../containers/journalists/JournalistDetailContainer.js";
-
-const DeleteJournalistButton = props => {
-  if(!props.journalistUrl) return null;
-  const url = props.journalistUrl;
-  console.log("deletebutton props", props);
-  return (
-    <div className = "delete-journalist-button">
-      <a href = {url} >Delete journalist</a>
-    </div>
-  )
-}
-
-export default DeleteJournalistButton;
+// import React from "react";
+// // import JournalistDetailContainer from "../../containers/journalists/JournalistDetailContainer.js";
+//
+// const DeleteJournalistButton = props => {
+//   if(!props.journalistUrl) return null;
+//   const url = props.journalistUrl;
+//   console.log("deletebutton props", props);
+//   return (
+//     <div className = "delete-journalist-button">
+//       <a href = {url} >Delete journalist</a>
+//     </div>
+//   )
+// }
+//
+// export default DeleteJournalistButton;
--- sgnn-admin/src/containers/journalists/JournalistDetailContainer.js
+++ sgnn-admin/src/containers/journalists/JournalistDetailContainer.js
@@ -3,50 +3,33 @@ import JournalistSingleDetail from '../../components/journalists/JournalistSingl
 import DeleteJournalistButton from "../../components/journalists/DeleteJournalistButton.js";
 import EditJournalistButton from "../../components/journalists/EditJournalistButton.js";
 import BackToAllButton from "../../components/journalists/BackToAllButton.js";
+import JournalistDeleteContainer from "./JournalistDeleteContainer.js";
 
 class JournalistDetailContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {journalist: null}
     this.url = props.url;
+    console.log("detail props", props);
   }
 
   componentDidMount(){
     fetch(this.url).then((res) => res.json())
     .then((data) => {
       this.setState({journalist: data})
+      // console.log("this.state.journalist", this.state.journalist);
     })
   }
 
-  // delete(journalistInfo){
-  //   console.log(journalistInfo);
-  //  const newState = this.state.journalist.slice();
-  //    if (newState.indexOf(journalistInfo) > -1) {
-  //    newState.splice(newState.indexOf(journalistInfo), 1);
-  //    this.setState({journalist: newState})
-  //  }
-  // }
-
-  // renderDelete(){
-  //   const listItem = this.state.data.map((item)=>{
-  //     return <div key={item.id}>
-  //       <span>{item.name}</span> <button onClick={this.delete.bind(this, item)}>Delete</button>
-  //     </div>
-  //   })
-  //   return <div>
-  //     {listItem}
-  //   </div>
-  // }
-
   render() {
+    if(!this.state.journalist) return null;
     return(
-      // add edit and remove button in this di
       <div className="journalist-container">
         <h1>Journalist info:</h1>
-
-        <DeleteJournalistButton journalistUrl = { this.url }/>
         <EditJournalistButton journalistUrl = { this.url }/>
         <JournalistSingleDetail journalist = { this.state.journalist } />
+        <JournalistDeleteContainer />
+        <br/>
         <BackToAllButton />
       </div>
     )
