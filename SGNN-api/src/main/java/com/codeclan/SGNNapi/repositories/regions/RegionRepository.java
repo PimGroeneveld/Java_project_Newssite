2b90bca42fd46a0a1081b24f4f6e205cf5240fb1
SitharaS <sithu_s@hotmail.com>
2018-10-21 08:48:03 +0100

    Fixes the query for region

--- SGNN-api/src/main/java/com/codeclan/SGNNapi/repositories/regions/RegionRepository.java
+++ SGNN-api/src/main/java/com/codeclan/SGNNapi/repositories/regions/RegionRepository.java
@@ -1,9 +1,11 @@
 package com.codeclan.SGNNapi.repositories.regions;
 
 import com.codeclan.SGNNapi.models.Region;
+import com.codeclan.SGNNapi.projections.RegionProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(excerptProjection = RegionProjection.class)
 public interface RegionRepository extends JpaRepository<Region, Long> {
 }
--- sgnn-admin/src/components/articles/Article.js
+++ sgnn-admin/src/components/articles/Article.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Article = (props) => {
 
   function handleRowClick(event){
@@ -12,7 +11,7 @@ const Article = (props) => {
           <td className="article-number-element"  value={props.article.id}>{props.index+1}</td>
           <td className="article-headline-element">{props.article.headline}</td>
           <td className="article-date-element">{props.article.publishDate}</td>
-          <td className="article-region-element">{props.article.region}</td>
+          <td className="article-region-element">{props.article.region.regionName}</td>
       </tr>
   )
 
