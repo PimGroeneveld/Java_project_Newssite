0998951689739040a4458ada5aa96d611ef1acb4
Robert <robert.deignan@outlook.com>
2018-10-21 11:14:39 +0100

    Add styling to region nav and dates

---
+++ sgnn-user/src/helpers/Date.js
@@ -0,0 +1,9 @@
+const prettyDate = (dateString) => {
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+                      "July", "August", "September", "October", "November", "December"];
+  const date = dateString.split("T")[0].split("-");
+  const time = dateString.split("T")[1].split(":");
+  return date[2] + " " + monthNames[date[1]] + " " + date[0] + " at " + time[0] + ":" + time[1];
+}
+
+export default prettyDate;
