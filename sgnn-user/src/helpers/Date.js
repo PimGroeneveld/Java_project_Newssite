5a4380c1721902b69d39fa4f696cb6ba1531dfe1
Robert <robert.deignan@outlook.com>
2018-10-23 10:11:37 +0100

    Fix date printing wrong month

--- sgnn-user/src/helpers/Date.js
+++ sgnn-user/src/helpers/Date.js
@@ -5,7 +5,7 @@ const prettyDate = (dateString) => {
                       "July", "August", "September", "October", "November", "December"];
   const date = dateString.split("T")[0].split("-");
   const time = dateString.split("T")[1].split(":");
-  return date[2] + " " + monthNames[date[1]] + " " + date[0] + " at " + time[0] + ":" + time[1];
+  return date[2] + " " + monthNames[date[1] - 1] + " " + date[0] + " at " + time[0] + ":" + time[1];
 }
 
 export default prettyDate;
